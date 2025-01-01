64db7005c4ec2b21aab6aeb50c18ae4955f2df59
Rakhmatilla Erkinov <erkinovit@gmail.com>
2025-01-01 21:27:11 +0500

    initial commit with simple malloc

---
+++ memalloc.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+void *malloc(size_t size) {
+    void *block;
+    block = sbrk(size);
+
+    if (block == (void*) -1) {
+        return NULL;
+    }
+    return block;
+}
